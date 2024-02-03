cf97db680034c579975b5f13f0ba49009f736f0d
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-03 02:44:39 +0100

    4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,10 @@
+-- create_table_script.sql
+
+-- Check if a database name is provided as an argument
+USE ?;
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
