2bcdf222b3e41ee86a7411f5ac9d0fcc6ab25d9c
YANZOOPA <yahia.ni.19@gmail.com>
2024-02-03 03:01:23 +0100

    fd

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,24 @@
 -- 5-table_description.sql
 
--- Retrieve column details from information schema
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+-- Set the database context
+USE ?;
+
+-- Retrieve table details from information schema
+SELECT CONCAT(
+    'CREATE TABLE `', TABLE_NAME, '` (\n',
+    GROUP_CONCAT(
+        '  `', COLUMN_NAME, '` ', COLUMN_TYPE,
+        IF(IS_NULLABLE = 'NO', ' NOT NULL', ''),
+        IF(COLUMN_DEFAULT IS NOT NULL, CONCAT(' DEFAULT ', QUOTE(COLUMN_DEFAULT)), ''),
+        '\n'
+        ORDER BY ORDINAL_POSITION SEPARATOR ''
+    ),
+    IFNULL(
+        CONCAT('  PRIMARY KEY (', GROUP_CONCAT('`', COLUMN_NAME, '`'), ')\n'),
+        ''
+    ),
+    ') ENGINE=', ENGINE, ' DEFAULT CHARSET=', CHARACTER_SET_NAME, ' COLLATE=', COLLATION_NAME
+) AS CreateTableStatement
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+WHERE TABLE_SCHEMA = ? AND TABLE_NAME = 'first_table'
+GROUP BY TABLE_NAME;
